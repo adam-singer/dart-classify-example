c109c8bb109c3d153050dc56b1a7e3848c28be23
adam <financeCoding@gmail.com>
2012-02-08 23:36:47 -0800

    adding code

---
+++ ClassifyExample.dart
@@ -0,0 +1,42 @@
+#import('dart:html');
+#import('../../dart_bleeding/dart/utils/dartdoc/classify.dart', prefix:"classify");
+
+class ClassifyExample {
+String code = """
+#import('dart:html');
+
+class Sababa {
+  /* [isCool] enables the cool pre tag */
+  ru([isCool = true]) {
+     var p;
+     if (cool) {
+        p = new Element.html('<pre>is cool cat</pre>');
+     } else {
+        p = new Element.html('<pre>is not cool cat</pre>');
+     }
+
+     // Add to body
+     document.body.nodes.add(p);
+  }
+}
+main() {
+  new Sababa().ru(ture);
+}
+""";
+
+  ClassifyExample() {
+  }
+
+  void run() {
+    classify.SourceFile sf = new classify.SourceFile("sf.dart", code);
+    String c = classify.classifySource(sf);
+    var e = new Element.html("<pre><source>${c}</source></pre>");
+    document.body.nodes.add(e);
+    //print(c);
+    
+  }
+}
+
+void main() {
+  new ClassifyExample().run();
+}
