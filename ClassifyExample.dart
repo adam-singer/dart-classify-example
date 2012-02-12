27df0c043e4b021cc10b2d16f8cd6345815368d7
adam <financeCoding@gmail.com>
2012-02-12 12:13:55 -0800

    Updating web page

--- ClassifyExample.dart
+++ ClassifyExample.dart
@@ -2,6 +2,7 @@
 #import('../../dart_bleeding/dart/utils/dartdoc/classify.dart', prefix:"classify");
 
 class ClassifyExample {
+String code2 = """ main() { print('helo'); } """;
 String code = """
 #import('dart:html');
 
@@ -31,7 +32,32 @@ main() {
     classify.SourceFile sf = new classify.SourceFile("sf.dart", code);
     String c = classify.classifySource(sf);
     var e = new Element.html("<pre><source>${c}</source></pre>");
-    document.body.nodes.add(e);
+    
+    TextAreaElement dartInput = document.query('#dartInput');
+    DivElement dartPreview = document.query('#dartPreview');
+    DivElement dartHTMLCode = document.query('#dartHTMLCode');
+    dartInput.value = code;
+    dartPreview.nodes.add(e);
+    var s = c.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+    var plainTextCode = new Element.html("<div style='word-wrap:break-word;'><pre><code>${s}</code></pre></div>");
+    dartHTMLCode.nodes.add(plainTextCode);
+    //print(s);
+    
+    dartInput.on.input.add((_) {
+      // Highlight code
+      sf = new classify.SourceFile("sf.dart", dartInput.value);
+      c = classify.classifySource(sf);
+      e = new Element.html("<pre><source>${c}</source></pre>");
+      s = c.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+      var plainTextCode = new Element.html("<div style='word-wrap:break-word;'><pre><code>${s}</code></pre></div>");
+      dartPreview.nodes.forEach((n)=>n.remove());
+      dartPreview.nodes.add(e);
+      dartHTMLCode.nodes.forEach((n)=>n.remove());
+      dartHTMLCode.nodes.add(plainTextCode);
+    });
+    
+    
+    
   }
 }
 
